5db784db67087fce2eaf1b108cd8c639f00782cd
xiaowangzi-02 <77796989+xiaowangzi-02@users.noreply.github.com>
2023-07-31 13:54:29 +0000

    test

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
 declare (strict_types=1);
-
+// mysqlsh --sql --host=zeabur-gcp-asia-east1-1.clusters.zeabur.com --port=31393 --user=root --password=6ZiK5P29eq07
 return [
     'driver' => 'mysql',
-    'host' => '127.0.0.1',
-    'database' => 'demo',
-    'username' => 'demo',
-    'password' => 'TbfXmL2JTcXYYrWZ',
+    'host' => 'zeabur-gcp-asia-east1-1.clusters.zeabur.com:31393',
+    'database' => 'faka',
+    'username' => 'root',
+    'password' => '6ZiK5P29eq07',
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'prefix' => 'acg_',
